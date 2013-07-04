633a2dea67e9f56e003315a12fde793de8b19e9a
Doug Orleans <dougorleans@gmail.com>
2013-07-04 00:33:56 -0400

    Add a singular route for a game's current state.

--- app/controllers/game_states_controller.rb
+++ app/controllers/game_states_controller.rb
@@ -3,4 +3,9 @@ class GameStatesController < ApplicationController
   inherit_resources
   belongs_to :game
   defaults :collection_name => :states
+
+  # For the singular path /games/:game_id/state, return the current game state.
+  def resource
+    params[:id] ? super : parent.state
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Letterclick::Application.routes.draw do
   resources :games, only: [:index, :create, :show] do
-    resources :states, controller: 'game_states', only: [:index, :create]
+    resources :states, controller: 'game_states', only: [:index]
+    resource :state, controller: 'game_states', only: [:show]
     resources :moves, only: [:index, :create]
   end
 end
